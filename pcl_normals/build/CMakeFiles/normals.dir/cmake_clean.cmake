7e4dbb27421a42ed996bcd33eadb023a070f3920
Gary <gary@mumec.com>
2017-11-07 21:30:46 -0800

    Progress on Plane detection

---
+++ pcl_normals/build/CMakeFiles/normals.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/normals.dir/src/get_normals.cpp.o"
+  "normals.pdb"
+  "normals"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/normals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
